742d74203ebec021f367cb1c728c516e3cb3a2c2
amgalan-sp <darzhaev@gmail.com>
2019-03-07 02:45:17 +0300

    Fixed code

--- spacegram.py
+++ spacegram.py
@@ -3,13 +3,12 @@ from instabot import Bot
 from dotenv import load_dotenv
 from fetch_spacex import fetch_spacex
 from fetch_hubble import get_collection
+import argparse
 
-
-def ensure_dir(foldername):
+def make_dir():
+    foldername = 'images'
+    os.makedirs(foldername)
     filepath = "{}/{}/".format(os.getcwd(), foldername)
-    directory = os.path.dirname(filepath)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
     os.chdir(filepath)
 
 
@@ -26,9 +25,19 @@ def upload_photos(extension='jpeg'):
 
 if __name__ == "__main__":
     load_dotenv()
-    ensure_dir('image')
-    fetch_spacex()
-    extension = str(input("введите расширение файлов для скачивания и загрузки "))
-    collection_name = str(input('введите название коллекции фотографий Хаббл '))
-    get_collection(collection_name, extension)
+    make_dir()
+    parser = argparse.ArgumentParser(description='This is a description')
+    parser.add_argument('-e','--extension', type=str, help='you can give extension(format) of a photo like jpeg, png, jpg, tiff' )
+    parser.add_argument('definite', help='you have 2 defs: spacex or hubble')
+    parser.add_argument('-c', '--collection', help='if you use hubble, yo need to input name of collection')
+    args = parser.parse_args()
+    collection_name = args.collection
+    extension = args.extension
+    definition = args.definite
+    if definition == 'spacex':
+        fetch_spacex()
+    elif definition == 'hubble':
+        get_collection(collection_name, extension)
+    else:
+        print('choose a definition: download spacex or huuble shoots')
     upload_photos()
